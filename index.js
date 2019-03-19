711f01de9fd5032bc988cfd877985310a51e8cc1
Peter Cunha <petercunha8@gmail.com>
2019-03-19 02:51:55 -0400

    Update credential lists, improve logging

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const VERSION = 0.1;
+const chalk = require('chalk')
 require('dotenv').config()
-let worker = require('./lib/job_queue')
+require('./lib/knock').init()
 
-worker.init()
+console.log(`
+_  ___   _  ___   ____ _  __
+| |/ / \\ | |/ _ \\ / ___| |/ /
+| ' /|  \\| | | | | |   | ' /
+| . \\| |\\  | |_| | |___| . \\
+|_|\\_\\_| \\_|\\___/ \\____|_|\\_'
+
+${chalk.dim('Knock v' + VERSION)}
+${chalk.dim('Hack the internet!')}
+${chalk.dim('Project by https://github.com/petercunha/knock')}
+`)
