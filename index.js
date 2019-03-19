341ff284e65f8ad620f0e6b16ef01f48fc8062d3
Peter Cunha <petercunha8@gmail.com>
2019-03-19 03:22:44 -0400

    Improve code comments

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const VERSION = 0.1;
+// Software Version
+const version = 0.1;
+
+// Initialize
 const chalk = require('chalk')
 require('dotenv').config()
 require('./lib/knock').init()
 
+// Banner
 console.log(`
 _  ___   _  ___   ____ _  __
 | |/ / \\ | |/ _ \\ / ___| |/ /
@@ -10,7 +14,7 @@ _  ___   _  ___   ____ _  __
 | . \\| |\\  | |_| | |___| . \\
 |_|\\_\\_| \\_|\\___/ \\____|_|\\_'
 
-${chalk.dim('Knock v' + VERSION)}
+${chalk.dim(`Knock v${version}`)}
 ${chalk.dim('Hack the internet!')}
 ${chalk.dim('Project by https://github.com/petercunha/knock')}
 `)
